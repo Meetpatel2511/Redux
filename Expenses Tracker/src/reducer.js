b234ac82b57335283cb5a0dbb63666710fc928c7
Meetpatel2511 <mp4297965@gmail.com>
2025-09-03 12:15:13 +0530

    Update reducer.js

--- Expenses Tracker/src/reducer.js
+++ Expenses Tracker/src/reducer.js
@@ -1,10 +1,10 @@
-export const addTransaction = (payload) => ({ type: "ADD", payload });
-export const deleteTransaction = (id) => ({ type: "DELETE", payload: id }); 
-export const editTransaction = (payload) => ({ type: "EDIT", payload });
-
 const initialState = { transactions: [], };
 
 export function reducer(state = initialState, action) { 
     switch (action.type) { case "ADD": return { ...state, transactions: [...state.transactions, action.payload] }; 
     case "DELETE": return { ...state, transactions: state.transactions.filter((t) => t.id !== action.payload), }; 
     case "EDIT": return { ...state, transactions: state.transactions.map((t) => t.id === action.payload.id ? { ...t, ...action.payload } : t ), }; default: return state; } }
+
+export const addTransaction = (payload) => ({ type: "ADD", payload });
+export const deleteTransaction = (id) => ({ type: "DELETE", payload: id }); 
+export const editTransaction = (payload) => ({ type: "EDIT", payload });
