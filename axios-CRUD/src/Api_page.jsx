b4b3b90342c8983d450db1961d013d4b06ae9b2a
Meetpatel2511 <mp4297965@gmail.com>
2025-09-09 10:30:38 +0530

    Update Api_page.jsx

--- axios-CRUD/src/Api_page.jsx
+++ axios-CRUD/src/Api_page.jsx
@@ -1,30 +1,22 @@
 import axios from "axios";
 
-// API instance
 const api = axios.create({
   baseURL: "https://dummyjson.com"
 });
 
-// ✅ GET - fetch all products
-// https://dummyjson.com/products
 export const getData = () => {
   return api.get("/products");
 };
 
-// ✅ DELETE - delete product by id
-// https://dummyjson.com/products/1
 export const deleteData = (id) => {
   return api.delete(`/products/${id}`);
 };
 
-// ✅ POST - add new product
-// https://dummyjson.com/products/add
 export const addData = (data) => {
   return api.post("/products/add", data);
 };
 
-// ✅ PUT - update product
-// https://dummyjson.com/products/1
 export const updateData = (id, updatedData) => {
   return api.put(`/products/${id}`, updatedData);
 };
+
